44262edeba81f7539908f0bcb02c64810d4408a6
Paul Schmidt <paul.schmidt@homechef.com>
2025-08-30 12:11:34 -0400

    add journal todos

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,10 +24,10 @@ load_dotenv(dotenv_path=env_path)
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# TODO: See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
