164408cae027a85506ccd5cb98c07be9b7eaf528
Paul Schmidt <paul.schmidt@homechef.com>
2025-10-25 11:06:59 -0400

    reset expirations for tokens

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -196,8 +196,8 @@ REST_FRAMEWORK = {
 
 # Simple JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,  # Generate new refresh token on refresh (throttled to 20/hour)
     'BLACKLIST_AFTER_ROTATION': True,  # Blacklist old refresh tokens after rotation (prevents token reuse attacks)
 
